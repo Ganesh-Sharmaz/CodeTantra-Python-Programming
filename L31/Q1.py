53e0c6cbdee2f620525f65e21a27fb40c4b876c0
Ganesh Sharma <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 23:17:48 +0530

    Create Q1.py

---
+++ L31/Q1.py
@@ -0,0 +1,7 @@
+strl = “This is my first String" 
+# print the string
+print(str1)
+# print Letter 'f' using Positive Index 
+print(str1[11])
+# print Letter 'S’ using Negative Index.
+print(str1[-6])
