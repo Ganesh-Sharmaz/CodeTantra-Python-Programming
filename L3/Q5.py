456eaa793cdb32e32a04e25ffc7223f9f99e8738
Ganesh-Sharmaz <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 15:36:23 +0530

    Create Q5.py

---
+++ L3/Q5.py
@@ -0,0 +1,8 @@
+strl = input(“Enter a value: ")
+
+a=b=c=strl
+
+# Assign str1 to three objects a, b and
+print("Value of a:", a ) # Print a
+print("Value of b:", a) # Print b
+print("Value of c:", a) # Print c
