1d770316b9517c03d610a4af943a39fb5a9ec2a8
Ganesh Sharma <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 22:20:30 +0530

    Create 2Q1.py

---
+++ L28/2Q1.py
@@ -0,0 +1,7 @@
+# Type Conversion Example
+
+a = "62"
+
+print(f" converting to int: {int(a)} ,data type: {type(int(a))}")
+print(f" converting to float: {float(a)} ,data type: {type(float(a))}")
+print(f" converting to complex: {complex(a)} ,data type: {type(complex(a))}")
