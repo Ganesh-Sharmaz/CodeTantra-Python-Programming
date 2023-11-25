2c42f8bc540c09b244b487c05feac6c8016a10a2
Ganesh Sharma <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 23:10:18 +0530

    Create Q3.py

---
+++ L30/Q3.py
@@ -0,0 +1,11 @@
+#import math module
+import math 
+print("constant\tvalue\tdata type")
+#print value and type of pi
+print("pi\t", math.pi , "\t", type(math.pi) )
+#print value and type of e 
+print("e\t", math.e , "\t", type(math.e) )
+#print value and type of inf 
+print("inf\t", "{:17}".format(math.inf), "\t", type(math.inf) )
+#print value and type of nan |
+print("NaN\t", "{:17}".format(math.nan), "\t", type(math.nan) )
