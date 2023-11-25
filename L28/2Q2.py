e08cae5460285226d24b6fd1049e9caef4c74457
Ganesh Sharma <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 22:22:46 +0530

    Create 2Q2.py

---
+++ L28/2Q2.py
@@ -0,0 +1,10 @@
+decimal = int(input("x: "))
+binary = bin(decimal)
+print(f"Decimal: {decimal} ")
+#convert into binary and print
+print(f"Binary: {binary} ")
+
+#convert into octal and print
+print(f"Octal: {oct(decimal)} ")
+#convert into hexadecimal and print
+print(f"Hexadecimal: {hex(decimal)}")
