dc9410658e9ee2dab42253950d5fcc58986cc0e3
Ganesh Sharma <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 22:17:03 +0530

    Create Q1.py

---
+++ L28/Q1.py
@@ -0,0 +1,12 @@
+# find quotient using ‘/' and '//' and remainder using '%"
+#like (a/b) and (a//b) and (a%b)
+
+a = int(input("a: "))
+
+b = int(input("b: "))
+
+print(f"{a} / {b} = {a/b}")
+
+print(f"{a} // {b} = {a//b}")
+
+print(f"{a} % {b} = {a%b}")
