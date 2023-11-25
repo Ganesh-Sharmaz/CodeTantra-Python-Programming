96fb29ec0e32d9d8f6d9c388d7d1680e54902337
Ganesh Sharma <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 21:54:35 +0530

    Create Q1.py

---
+++ L4/Q1.py
@@ -0,0 +1,17 @@
+numberl = 20.50
+
+number2 = 38.25
+
+# Print the multiplication of number1 and number2 using * operator.
+
+h1 = number1*number2
+
+print(h1)
+
+string1 = "Amazon"
+
+string2 = "River"
+
+print(string1+string2)
+
+# Print the concatenated value of string1 and string2 using + operator.
