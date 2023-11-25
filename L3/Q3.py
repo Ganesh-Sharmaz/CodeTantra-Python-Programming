29d1524f71bcfc7f71f9e21392be32bcff5e65df
Ganesh-Sharmaz <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 15:32:37 +0530

    Create Q3.py

---
+++ L3/Q3.py
@@ -0,0 +1,5 @@
+kilometers - int(input(“Enter a value: )) # assign the correct value
+convertfactor = 0.621371 # assign the correct value
+
+miles - kilometers * convertfactor # multiply kilometers and convertfactor
+print("Miles:", miles)
