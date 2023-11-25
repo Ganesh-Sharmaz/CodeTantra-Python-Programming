68dc014bab7c596cc64175fa112801fc62446072
Ganesh-Sharmaz <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 15:26:19 +0530

    Create Q4.py

---
+++ L2/Q4.py
@@ -0,0 +1,8 @@
+# In the below line, assign value 18 to the length variable
+length = 15
+
+# In the below Line, assign value "King Cobra” to the snake variable
+snake = "King Cobra’
+
+
+print(snake, "can grow up to a length of", length, "feet")
