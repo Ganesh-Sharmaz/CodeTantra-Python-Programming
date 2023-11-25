93f8837b66f5b2963819c010df3f5aa821f126bf
Ganesh Sharma <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 22:57:31 +0530

    Create Q2.py

---
+++ L30/Q2.py
@@ -0,0 +1,7 @@
+# Write a program to print Mathematical constants
+
+print("pi\t", math.pi, "\t", type(math.pi) ) # find type of math.pi using type function
+print("e\t”, math.e, "\t", type(math.e) )# find type of math.e
+
+print("inf\t", "{:17}".format(math.inf), "\t", type(math.inf) ) # find type of math.inf
+print(“NaN\t", "{:17}".format(math.nan), "\t", type(math.nan) ) # find type of math.nan
