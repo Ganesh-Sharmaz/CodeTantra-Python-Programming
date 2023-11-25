0555ce361c16fb17f3c081e942d1a5a3a25bb1f5
Ganesh-Sharmaz <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 15:17:51 +0530

    Create Q2.py

---
+++ L2/Q2.py
@@ -0,0 +1,19 @@
+#print("1") or u can use shortcut go to the end of comment and click ctrl+/ and it'll remove the hashtag and you don't have to type the entire code
+print("1")
+
+# print("2")
+print("3")
+
+# print("4”)
+print("5")
+
+# print("6")
+print("7")
+
+# print("8")
+print("9")
+
+# print("107)
+print("11")
+# print("12")
+print("13")
