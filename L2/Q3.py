adab571f462c284b1315396074ac4e239403983d
Ganesh-Sharmaz <151487165+Ganesh-Sharmaz@users.noreply.github.com>
2023-11-25 15:21:22 +0530

    Create Q3.py

---
+++ L2/Q3.py
@@ -0,0 +1,10 @@
+def add(a, b):
+"""Return sum of given arguments"""
+    return a + b
+def power(b, e):
+"""Return the power value.
+b —- is the base
+e —- is the exponent"""
+    return b ** e
+print(add.__doc__)
+print (power.__doc__)
